a557d77d011c4ff1df5f324d5b373993ad538a8b
Felix Buschbeck <felixb15@proton.me>
2025-11-21 09:05:55 +0100

    Add delete button to variable window

--- src/main/java/de/usd/cstchef/view/VariablesWindow.java
+++ src/main/java/de/usd/cstchef/view/VariablesWindow.java
@@ -1,13 +1,17 @@
-package de.usd.cstchef.view;
+package de.usd.cstchef.view; 
 
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.TreeMap; 
 
+import javax.swing.AbstractCellEditor;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -15,13 +19,14 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumn; 
 
 import burp.api.montoya.core.ByteArray;
+import de.usd.cstchef.VariableStore; 
 
 public class VariablesWindow extends JFrame {
-
     private static VariablesWindow instance;
 
     public static VariablesWindow getInstance() {
@@ -38,10 +43,12 @@ public class VariablesWindow extends JFrame {
         super("Variables");
         this.setSize(new Dimension(600, 480));
 
-        DefaultTableModel model = new DefaultTableModel(new String[] { "Variable Name", "Content" }, 0);
+        // Added third column for the Delete button
+        DefaultTableModel model = new DefaultTableModel(new String[] { "Variable Name", "Content", "Delete" }, 0);
         this.table = new JTable(model) {
+            // Only the delete column should be editable so its button can be clicked
             public boolean isCellEditable(int row, int column) {
-                return false;
+                return column == 2;
             };
         };
 
@@ -57,10 +64,17 @@ public class VariablesWindow extends JFrame {
         this.table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         this.table.getColumnModel().getColumn(0).setPreferredWidth(200);
         this.table.getColumnModel().getColumn(1).setCellRenderer(new WordWrapCellRenderer());
+
+        // Setup the Delete button column renderer and editor
+        TableColumn deleteColumn = this.table.getColumnModel().getColumn(2);
+        deleteColumn.setCellRenderer(new ButtonRenderer());
+        deleteColumn.setCellEditor(new ButtonEditor(this.table));
+        deleteColumn.setPreferredWidth(80);
+
         this.table.getTableHeader().setReorderingAllowed(false);
         this.table.getTableHeader().setResizingAllowed(false);
         this.table.setFillsViewportHeight(true);
-        ((DefaultTableCellRenderer)table.getTableHeader().getDefaultRenderer()).setHorizontalAlignment(JLabel.LEFT);
+        ((DefaultTableCellRenderer) table.getTableHeader().getDefaultRenderer()).setHorizontalAlignment(JLabel.LEFT);
 
         this.emptyLbl = new JLabel("no variables defined");
         this.table.add(this.emptyLbl);
@@ -76,23 +90,83 @@ public class VariablesWindow extends JFrame {
         SortedMap<String, ByteArray> sortedMap = new TreeMap<String, ByteArray>(variables);
 
         for (String key : sortedMap.keySet()) {
-            model.addRow(new String[] { key, sortedMap.get(key).toString() });
+            // Third column holds the button label; actual button rendering/editor handles actions
+            model.addRow(new Object[] { key, sortedMap.get(key).toString(), "Delete" });
         }
     }
 
     private void setColumnWidth(Dimension preferredSize) {
         TableColumn contentColumn = this.table.getColumnModel().getColumn(1);
-        int parentWidth = this.table.getParent().getWidth();
-        int width = Integer.max(preferredSize.width + WordWrapCellRenderer.MARGIN, parentWidth - this.table.getColumnModel().getColumn(0).getWidth());
-        contentColumn.setPreferredWidth(width);
+        int parentWidth = 0;
+        if (this.table.getParent() != null) {
+            parentWidth = this.table.getParent().getWidth();
+        }
+        int width = Integer.max(preferredSize.width + WordWrapCellRenderer.MARGIN,
+            parentWidth - this.table.getColumnModel().getColumn(0).getWidth() - this.table.getColumnModel().getColumn(2).getWidth());
+        contentColumn.setPreferredWidth(Math.max(width, 50));
+    }
+
+    // Button renderer for Delete column
+    private class ButtonRenderer extends JButton implements TableCellRenderer {
+        public ButtonRenderer() {
+            setText("Delete");
+            setOpaque(true);
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+            boolean hasFocus, int row, int column) {
+            return this;
+        }
+    }
+
+    // Button editor for Delete column
+    private class ButtonEditor extends AbstractCellEditor implements TableCellEditor, ActionListener {
+        private final JButton button;
+        private final JTable tableRef;
+        private int currentRow = -1;
+
+        public ButtonEditor(JTable table) {
+            this.tableRef = table;
+            this.button = new JButton("Delete");
+            this.button.addActionListener(this);
+        }
+
+        @Override
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+            int column) {
+            this.currentRow = row;
+            return this.button;
+        }
+
+        @Override
+        public Object getCellEditorValue() {
+            return "Delete";
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Stop editing first to commit changes and close editor
+            stopCellEditing();
+            DefaultTableModel model = (DefaultTableModel) tableRef.getModel();
+            if (currentRow >= 0 && currentRow < model.getRowCount()) {
+                // Remove variable from variable store
+                VariableStore.getInstance().removeVariable((String) model.getValueAt(currentRow, 0));
+                model.removeRow(currentRow);
+                // Update empty label visibility if needed
+                emptyLbl.setVisible(model.getRowCount() == 0);
+            }
+        }
     }
 
     class WordWrapCellRenderer extends JTextArea implements TableCellRenderer {
         private static final int MARGIN = 20;
 
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+            boolean hasFocus, int row, int column) {
+            // set text first then compute preferred size
+            setText(value == null ? "" : value.toString());
             Dimension preferredSize = getPreferredSize();
-            setText(value.toString());
             setSize(preferredSize.width, getPreferredSize().height);
             if (table.getRowHeight(row) != getPreferredSize().height) {
                 table.setRowHeight(row, getPreferredSize().height);
@@ -101,4 +175,4 @@ public class VariablesWindow extends JFrame {
             return this;
         }
     }
-}
+} 
