a0c9bdc547eb33981535cad2f8b43abdd89f3bc3
Felix Buschbeck <felixb15@proton.me>
2026-06-25 11:36:40 +0200

    Change repeater CSTC tab to use current filter state

--- src/main/java/burp/MyExtensionProvidedHttpRequestEditor.java
+++ src/main/java/burp/MyExtensionProvidedHttpRequestEditor.java
@@ -2,6 +2,7 @@ package burp;
 
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.core.ByteArray;
+import burp.api.montoya.core.ToolType;
 import burp.api.montoya.http.message.HttpRequestResponse;
 import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.ui.Selection;
@@ -9,9 +10,16 @@ import burp.api.montoya.ui.editor.EditorOptions;
 import burp.api.montoya.ui.editor.RawEditor;
 import burp.api.montoya.ui.editor.extension.EditorCreationContext;
 import burp.api.montoya.ui.editor.extension.ExtensionProvidedHttpRequestEditor;
+import de.usd.cstchef.view.RecipePanel;
 import de.usd.cstchef.view.View;
+import de.usd.cstchef.view.filter.FilterState;
 
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class MyExtensionProvidedHttpRequestEditor implements ExtensionProvidedHttpRequestEditor
 {
@@ -19,12 +27,15 @@ public class MyExtensionProvidedHttpRequestEditor implements ExtensionProvidedHt
     private HttpRequestResponse requestResponse;
     private final MontoyaApi api;
     private final View view;
+    private final ToolType toolType;
 
     MyExtensionProvidedHttpRequestEditor(EditorCreationContext creationContext, View view)
     {
         this.api = BurpUtils.getInstance().getApi();
         this.view = view;
+        this.toolType = creationContext.toolSource().toolType();
         requestEditor = api.userInterface().createRawEditor(EditorOptions.READ_ONLY);
+        requestEditor.uiComponent().addFocusListener(new CstcFocusRequestListener(this));
     }
 
     @Override
@@ -36,8 +47,30 @@ public class MyExtensionProvidedHttpRequestEditor implements ExtensionProvidedHt
     @Override
     public void setRequestResponse(HttpRequestResponse requestResponse)
     {
-        ByteArray result = view.getRecipePanelAtIndex(0).bake(requestResponse.request().toByteArray(), requestResponse.request().toByteArray());
+        this.requestResponse = requestResponse;
+
+        if (requestResponse == null || requestResponse.request() == null) {
+            this.requestEditor.setContents(ByteArray.byteArray());
+            return;
+        }
+
+        ByteArray originalRequest = requestResponse.request().toByteArray();
+        ByteArray result = originalRequest;
+
+        for (RecipePanel recipePanel : getOrderedRecipePanels(FilterState.BurpOperation.OUTGOING, toolType)) {
+            result = recipePanel.bake(result, originalRequest);
+        }
+
         this.requestEditor.setContents(result);
+    }
+
+    public void reapplyRecipe()
+    {
+        if (this.requestResponse == null) {
+            return;
+        }
+
+        this.setRequestResponse(this.requestResponse);
     }
 
     @Override
@@ -68,5 +101,90 @@ public class MyExtensionProvidedHttpRequestEditor implements ExtensionProvidedHt
     public boolean isModified()
     {
         return requestEditor.isModified();
+    }
+
+    private List<RecipePanel> getOrderedRecipePanels(FilterState.BurpOperation operation, ToolType toolType)
+    {
+        List<OrderedRecipePanel> orderedRecipePanels = new ArrayList<>();
+        int recipeIndex = 0;
+
+        for (RecipePanel recipePanel : view.getFilterableRecipePanels()) {
+            if (!recipePanel.getOperation().equals(operation)) {
+                recipeIndex++;
+                continue;
+            }
+
+            int selectionOrder = BurpUtils.getInstance().getFilterState()
+                    .getFilterSelectionOrder(operation, recipePanel.getRecipeName(), toolType);
+            if (selectionOrder <= 0) {
+                recipeIndex++;
+                continue;
+            }
+
+            orderedRecipePanels.add(new OrderedRecipePanel(recipePanel, selectionOrder, recipeIndex));
+            recipeIndex++;
+        }
+
+        orderedRecipePanels.sort(Comparator
+                .comparingInt(OrderedRecipePanel::getSelectionOrder)
+                .thenComparingInt(OrderedRecipePanel::getRecipeIndex));
+
+        List<RecipePanel> sortedRecipePanels = new ArrayList<>();
+        for (OrderedRecipePanel orderedRecipePanel : orderedRecipePanels) {
+            sortedRecipePanels.add(orderedRecipePanel.getRecipePanel());
+        }
+
+        return sortedRecipePanels;
+    }
+
+    private static class OrderedRecipePanel
+    {
+        private final RecipePanel recipePanel;
+        private final int selectionOrder;
+        private final int recipeIndex;
+
+        private OrderedRecipePanel(RecipePanel recipePanel, int selectionOrder, int recipeIndex)
+        {
+            this.recipePanel = recipePanel;
+            this.selectionOrder = selectionOrder;
+            this.recipeIndex = recipeIndex;
+        }
+
+        private RecipePanel getRecipePanel()
+        {
+            return recipePanel;
+        }
+
+        private int getSelectionOrder()
+        {
+            return selectionOrder;
+        }
+
+        private int getRecipeIndex()
+        {
+            return recipeIndex;
+        }
+    }
+
+    private static class CstcFocusRequestListener implements FocusListener
+    {
+        private final MyExtensionProvidedHttpRequestEditor requestEditor;
+
+        private CstcFocusRequestListener(MyExtensionProvidedHttpRequestEditor requestEditor)
+        {
+            this.requestEditor = requestEditor;
+        }
+
+        @Override
+        public void focusGained(FocusEvent e)
+        {
+            this.requestEditor.reapplyRecipe();
+        }
+
+        @Override
+        public void focusLost(FocusEvent e)
+        {
+            // Not needed
+        }
     }
 }
